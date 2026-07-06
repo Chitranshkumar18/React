d6a0ef0dc1898caddf886360491241a0297af8ef
Chitransh kumar <chitranshkumar@Chitranshs-MacBook-Air.local>
2026-07-06 17:08:29 +0530

    hooks

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const addValue = () => {
+    if (count < 20) {
+      setCount(count + 1);
+    }
+  };
+
+  const removeValue = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  return (
+    <>
+      <h1>Hello React!</h1>
+
+      <h2>Counter Value: {count}</h2>
+
+      <button onClick={addValue}>
+        Add Value {count}
+      </button>
+
+      <br />
+      <br />
+
+      <button onClick={removeValue}>
+        Remove Value {count}
+      </button>
+
+      <p>Footer: {count}</p>
+    </>
+  );
+}
+
+export default App
