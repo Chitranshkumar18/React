a41eedaa0070e6254a3c859d2d5a24c74c2d9625
Chitransh kumar <chitranshkumar@Chitranshs-MacBook-Air.local>
2026-07-06 12:12:03 +0530

    customreact

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 
-import App from './App.jsx'
+import App from './App.jsx';
+
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom App | Chai</h1>
+    </div>
+  );
+}
+
+const anotherElement = (
+  <a href="https://google.com" target="_blank" rel="noreferrer">
+    Visit Google
+  </a>
+);
+
+const anotherUser = "chai aur react";
+
+const ReactElement = React.createElement(
+  'a',
+  {
+    href: 'https://google.com',
+    target: '_blank',
+    rel: 'noreferrer',
+  },
+  `Click me to visit Google ${anotherUser}`
+);
 
 createRoot(document.getElementById('root')).render(
-
-    <App />
-  
-)
+  ReactElement
+);
