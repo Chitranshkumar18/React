6afe9f59dec6135ff3e42013cab09433c6ad824a
Chitranshkumar18 <chitranshkumar730@gmail.com>
2026-08-21 13:46:27 +0530

    React Router Project

--- 07reactrouter/src/Layout.jsx
+++ 07reactrouter/src/Layout.jsx
@@ -14,3 +14,7 @@ function Layout() {
 }
 
 export default Layout
+
+
+
+//Outlet react-router-dom ka ek component hai jo nested routes ke liye placeholder ka kaam karta hai — matlab parent route ke andar child route kahan render hogi, wo jagah batata hai.
