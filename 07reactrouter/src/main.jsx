2bbc4a1da59d887af52b8e705c1e7451827b702c
Chitranshkumar18 <chitranshkumar730@gmail.com>
2026-08-21 13:34:30 +0530

    React router project

--- 07reactrouter/src/main.jsx
+++ 07reactrouter/src/main.jsx
@@ -53,3 +53,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <RouterProvider router={router} />
   </React.StrictMode>,
 )
+
+
+//Router ka kaam hota hai URL ke according different page/component dikhana.
