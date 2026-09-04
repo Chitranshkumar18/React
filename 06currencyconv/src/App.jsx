cf2714f2f266cc43bac4ffbb16d32bd327e7e658
ChitranshKumar18 <chitranshkumar730@gmail.com>
2026-09-05 03:49:03 +0530

    change

--- 06currencyconv/src/App.jsx
+++ 06currencyconv/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import {InputBox} from './Component'
-import useCurrencyInfo from './hooks/useCurrencyInfo.'
-
+import useCurrencyInfo from "../hooks/useCurrencyInfo";
 
 
 function App() {
